0c8cd2bcfb43ce8464973008b0f10e9abdb54aef
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2013-02-05 12:53:51 -0800

    Update cookbooks/splunk/recipes/default.rb

--- cookbooks/splunk/recipes/default.rb
+++ cookbooks/splunk/recipes/default.rb
@@ -15,14 +15,14 @@ user "splunk" do
 end
 
 
-directory "/opt/splunk" do
+directory "/opt/cheftraining" do
  owner "splunk"
  mode "0755"
  action :create
 end
 
-remote_file "/opt/splunk.tar" do
- source "https://rs-training-assets.s3.amazonaws.com/splunk-4.3.4-136012-Linux-x86_64.tar"
+remote_file "/opt/cheftraining/splunk.tar" do
+ source "https://rsed-cheftraining.s3.amazonaws.com/splunk-4.3.4-136012-Linux-x86_64.tar"
  action :create_if_missing
 end
 
@@ -30,7 +30,7 @@ bash "install_splunk" do
  cwd "/opt"
  code <<-EOH
   tar -xvf splunk.tar
-  chown -R splunk /opt/splunk
+  chown -R splunk /opt/cheftraining
   EOH
 end
 
