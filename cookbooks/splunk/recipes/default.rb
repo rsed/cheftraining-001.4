d598e5aaa839bbb97035d2fbb7ac988467915b34
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2013-02-11 17:03:49 +0000

    Update cookbooks/splunk/recipes/default.rb

--- cookbooks/splunk/recipes/default.rb
+++ cookbooks/splunk/recipes/default.rb
@@ -7,31 +7,3 @@
 # All rights reserved - Do Not Redistribute
 #
 
-log "Installing Splunk version 4"
-
-user "splunk" do
- action :create
- shell "/bin/bash"
-end
-
-
-directory "/opt/cheftraining" do
- owner "splunk"
- mode "0755"
- action :create
-end
-
-remote_file "/opt/cheftraining/splunk.tar" do
- source "https://rsed-cheftraining.s3.amazonaws.com/splunk-4.3.4-136012-Linux-x86_64.tar"
- action :create_if_missing
-end
-
-bash "install_splunk" do
- cwd "/opt"
- code <<-EOH
-  tar -xvf splunk.tar
-  chown -R splunk /opt/cheftraining
-  EOH
-end
-
-package "tree"
